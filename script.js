fde6913ffdb0cc0461ee34827a5d5a2aa1a7e9de
Jeet Debnath <jeet.debnath2004@gmail.com>
2023-07-21 01:11:57 +0530

    added basic functioning

---
+++ script.js
@@ -0,0 +1,48 @@
+const draw_width = 500
+const draw_height = 500
+
+const draw_area = document.querySelector('.draw-area')
+
+draw_area.style.width = draw_width + "px"
+draw_area.style.height = draw_height + "px"
+
+
+
+
+const slider = document.querySelector(".slider")
+
+const sliderValueElement = document.querySelector(".value")
+
+let sliderValue = 1;
+
+updateSliderValue();
+
+sliderValueElement.textContent = sliderValue + " X " + sliderValue;
+
+function updateSliderValue() {
+    draw_area.replaceChildren();
+    
+    sliderValue = slider.value;
+    sliderValueElement.textContent = sliderValue + " X " + sliderValue;
+
+    let boxes = sliderValue
+
+    const total_boxes = boxes * boxes
+
+    const box_width = (draw_height / boxes)
+
+    for (let i = 0; i < total_boxes; i++) {
+        const box = document.createElement('div')
+        box.classList.add("grid-box")
+        draw_area.appendChild(box)
+        box.style.width = box_width + "px"
+        box.style.height = box_width + "px"
+    }
+
+    const box = document.querySelector(".grid-box")
+
+
+}
+
+slider.addEventListener("change", updateSliderValue);
+
